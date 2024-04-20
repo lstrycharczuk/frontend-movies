7814dc58ed40eb5c58dc080137754bd30af27f1f
Laura Strycharczuk <u21_laustr_lbn@technischools.com>
2024-04-20 17:46:09 +0200

    components added

--- src/components/SingleMovie.jsx
+++ src/components/SingleMovie.jsx
@@ -1,5 +1,4 @@
 import { useParams } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import useFetch from "react-fetch-hook";
